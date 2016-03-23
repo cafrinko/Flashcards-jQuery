99db5336df158cfe940404ba471551b3764a96b1
Catherine Karra and Donna Ditz <donna@>
2016-03-22 17:41:58 -0700

    finished flashcard study guide

---
+++ js/flashcardscripts.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+  $(".javascriptdefinition").click(function() {
+    $("#js").slideToggle();
+    $("#jsdef").slideToggle();
+  });
+
+  $(".jquerydefinition").click(function() {
+    $("#jquery").slideToggle();
+    $("#jquerydef").slideToggle();
+  });
+
+});
